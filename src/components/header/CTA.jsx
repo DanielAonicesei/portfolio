f8d40e82e040829b5d05bb443ccb5d87944aca77
DanielAonicesei <aoni.daniel@gmail.com>
2022-11-06 18:44:44 +0200

    changes

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import DanielCV from '../../assets/DanielCV.pdf';
+import PersonalCV from '../../assets/PersonalCV.pdf';
 
 const CTA = () => {
   return (
     <div className="cta">
-        <a href={DanielCV} download className='btn'>Download CV</a>
+        <a href={PersonalCV} download className='btn'>Download CV</a>
         <a href="#contact" className='btn btn-primary'>Let's Talk</a>
     </div>
   )
